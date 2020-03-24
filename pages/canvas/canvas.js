8a85f68f9ceeafc2397bc50a15e548e72fa6f10b
more-ka <1640627628@qq.com>
2020-03-24 16:37:29 +0800

    点击分享按钮预览照片,长按发送给朋友

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -9,13 +9,15 @@ Page({
     topMargin: 0,
     imgInfo: {},
     ctx: [1, 2, 3],
+    canvasImage: '',
+    previewImage: false,
     imgProportion: 0.8, // 图片占canvas画布宽度百分比, 不建议随意修改
     imgToTop: 100 // 图片到canvas顶部的距离
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
     var sysInfo = wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         that.setData({
           canvasWidth: res.windowWidth,
           canvasHeight: res.windowHeight * 0.8
@@ -27,7 +29,7 @@ Page({
       }
     })
   },
-  start: function () {
+  start: function() {
     let that = this
     let ctx = wx.createCanvasContext('myCanvas')
     ctx.setFillStyle('#f8f8f8')
@@ -38,7 +40,7 @@ Page({
     this.addImage(ctx)
   },
   // 选择图片
-  addImage: function (ctx) {
+  addImage: function(ctx) {
     var context = wx.createContext();
     var that = this;
     let imgInfo = that.data.imgInfo
@@ -48,7 +50,7 @@ Page({
       success(res) {
         wx.getImageInfo({
           src: res.tempFilePaths[0],
-          success: function (response) {
+          success: function(response) {
             that.setData({
               imgInfo: response,
               path: res.tempFilePaths[0]
@@ -68,12 +70,12 @@ Page({
     let path = that.data.path
     let imgWidth = that.data.canvasWidth * that.data.imgProportion
     let imgHeight = imgInfo.height / imgInfo.width * imgWidth
-    console.log('imgwidth', imgWidth, 'imgHeight', imgHeight)
     ctx.drawImage(path, 0, 0, imgInfo.width, imgInfo.height, that.data.leftMargin, that.data.imgToTop, imgWidth, imgHeight)
     ctx.draw()
+    that.data.previewImage = true
   },
   //画标题
-  addTitle: function (ctx) {
+  addTitle: function(ctx) {
     var str = this.data.title
     ctx.font = 'normal bold 16px sans-serif';
     ctx.setTextAlign('center'); // 文字居中
@@ -83,10 +85,10 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     // return eventHandler接收到的分享参数
     console.log('fengxinagshijian')
-    console.log('share',this.data.path,this.data.title)
+    console.log('share', this.data.path, this.data.title)
     return {
       title: this.data.title,
       path: '/pages/canvas/canvas',
@@ -94,39 +96,66 @@ Page({
     };
   },
   //点击下载按钮保存canvas图片
-  downloadCanvas: function () {
+  downloadCanvas: function() {
     let that = this;
+    if (that.data.previewImage) {
+      wx.canvasToTempFilePath({
+        x: 0,
+        y: 0,
+        width: that.canvasWidth,
+        height: that.canvasWidth,
+        destWidth: that.canvasWidth,
+        destHeight: that.canvasHeight,
+        canvasId: 'myCanvas',
+        success: function success(res) {
+          wx.saveImageToPhotosAlbum({
+            filePath: res.tempFilePath,
+            success(res) {
+              console.log(res, '保存')
+            }
+          })
+        }
+      });
+    } else {
+      wx.showToast({
+        title: '请先选择图片',
+        image: '../../static/img/error.png'
+      })
+    }
+  },
+  // 分享图片
+  share() {
+    let that = this
+    if (that.data.previewImage) {
+
     wx.canvasToTempFilePath({
       x: 0,
       y: 0,
-      width: that.canvasWidth * 2,
-      height: that.canvasWidth * 2,
-      destWidth: that.canvasWidth * 2,
-      destHeight: that.canvasHeight * 2,
+      width: that.canvasWidth,
+      height: that.canvasWidth,
+      destWidth: that.canvasWidth,
+      destHeight: that.canvasHeight,
       canvasId: 'myCanvas',
       success: function success(res) {
-        wx.saveImageToPhotosAlbum({
-          filePath: res.tempFilePath,
-          success(res) { 
-            console.log(res,'保存')
-          }
-        })
-        // wx.downloadFile({
-        //   tempFilePath: res.tempFilePath,
-        //   success: function success(res) {
-        //     that.setData({
-        //       imagePath: res.savedFilePath
-        //     });
-        //     console.log(res.savedFilePath)
-
-        //   }
-        // });
+          wx.showToast({
+            icon: 'none',
+            title: '长按图片分享',
+            duration: 1500
+          })
+          setTimeout(
+            () => {
+              wx.previewImage({
+                urls: [res.tempFilePath]
+              })
+            }, 1000)
       }
-    });
-  },
-  // 分享图片
-  share(){
-
+    })
+    } else {
+      wx.showToast({
+        title: '请先选择图片',
+        image: '../../static/img/error.png'
+      })
+    }
   },
   saveImg() {
     let that = this;
